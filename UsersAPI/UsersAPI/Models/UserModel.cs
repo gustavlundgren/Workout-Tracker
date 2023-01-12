a353856c64a45a26d9f90ea8e0ea1f6904ef6921
gustavlundgren <sgvln005@edu.karlstad.se>
2023-01-12 20:07:42 +0100

    API include

---
+++ UsersAPI/UsersAPI/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsersAPI.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string Username { get; set; }
+
+        [Column(TypeName ="nvarchar(250)")]    
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Password { get; set; }
+    }
+}
